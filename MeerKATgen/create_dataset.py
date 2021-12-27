464af4879c316b21737083b3894d18e0628b2eda
petchma <peterxiangyuanma@gmail.com>
2021-12-26 19:56:00 -0800

    done functionality

---
+++ MeerKATgen/create_dataset.py
@@ -0,0 +1,171 @@
+import numpy as np
+from jax import vmap, jit
+
+from .observation import Observation
+from .sim_params import random_SETI_params, random_RFI_params 
+from .sim_params import blank_RFI_params, blank_SETI_params 
+
+def create_simulated_obs(num_beams,fchans, tchans, telescope_sigma , SETI, RFI, obs_data= None):
+    if obs_data == None: 
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=None)
+    else:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=obs_data)
+    return [obs.extract_all()]
+
+def create_simulated_obs_true_simulated(num_beams=64,fchans =256, tchans =16, obs_data=None,telescope_sigma=0.5, index=0):
+    RFI = random_RFI_params()
+    SETI = random_SETI_params()   
+    if obs_data:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=obs_data)
+    else:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=None)
+    return [obs.extract_all()]
+
+
+def create_simulated_obs_single_true(num_beams,fchans, tchans, telescope_sigma , SETI, obs_data=None, index=0):
+    RFI = blank_RFI_params()
+    if obs_data:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=obs_data)
+    else:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=None)
+    return [obs.extract_all()]
+
+
+def create_simulated_obs_single_true_simulated(num_beams=64,fchans =256, tchans =16, obs_data=None, telescope_sigma=0.5, index=0):
+    RFI = blank_RFI_params()
+    SETI = random_SETI_params()   
+    if obs_data:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=obs_data)
+    else:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=None)
+    return [obs.extract_all()]
+
+def create_simulated_obs_false(num_beams,fchans, tchans, telescope_sigma , RFI, obs_data = None, index=0):
+    SETI = blank_SETI_params()
+    if obs_data:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=obs_data)
+    else:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=None)
+    return [obs.extract_all()]
+
+def create_simulated_obs_false_simulated(num_beams=64,fchans =256, tchans =16, obs_data=None,telescope_sigma=0.5, index=0):
+    RFI = random_RFI_params()
+    SETI = blank_SETI_params()
+    if obs_data:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=obs_data)
+    else:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=None)
+    return [obs.extract_all()]
+
+
+def create_simulated_obs_false_empty(num_beams,fchans, tchans, telescope_sigma , RFI, obs_data=None, index=0):
+    SETI = blank_SETI_params()
+    RFI = blank_RFI_params()
+    if obs_data:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=obs_data)
+    else:
+        obs = Observation(num_beams=num_beams,
+                    fchans=fchans,
+                    tchans=tchans,
+                    ascending=False,
+                    telescope_sigma = telescope_sigma,
+                    RFI = RFI,
+                    SETI = SETI,
+                    obs_data=None)
+    return [obs.extract_all()]
+
+
+def vectorize(num, func):
+    iteration = np.ones((num))
+    return vmap(func, in_axes =(None, None, None,None,None, 0))(iteration)
--- execute_test.py
+++ execute_test.py
@@ -1,28 +1,18 @@
 from MeerKATgen import Observation
-
+from MeerKATgen.sim_params import random_SETI_params, random_RFI_params 
+from MeerKATgen.create_dataset import create_simulated_obs_true_simulated
+from MeerKATgen.create_dataset import create_simulated_obs_single_true_simulated
+from MeerKATgen.create_dataset import create_simulated_obs_false_simulated
+from MeerKATgen.create_dataset import vectorize
 """
 Tests to run with github actions  
-
-
 """
 
-rfi_location = [[10,10],[100,100]]
-rfi_deviation = [10,10]
-rfi_start_index = [40,200]
-rfi_snr = [50,40]
-rfi_drift = [1,-1]
-rfi_width = [10,5]
-rfi_mean = 10
-RFI = [rfi_location, rfi_deviation, rfi_start_index, rfi_snr, rfi_drift,  rfi_width, rfi_mean]
 
-
-SETI_INDEX = [0,20]
-seti_start_index = [100,150]
-seti_snr = [30,30]
-seti_drift = [-2,2]
-seti_width = [30,30]
-SETI = [SETI_INDEX, seti_start_index, seti_snr, seti_drift,  seti_width]
-
+print("Test Sim parameters")
+RFI = random_RFI_params(10)
+SETI = random_SETI_params(2)
+print("Test Blank Obs")
 obs = Observation(num_beams=64,
                  fchans=256,
                  tchans=16,
@@ -36,25 +26,9 @@ data, coordinates, adj_matrix, labels  = obs.extract_all()
 
 
 obs_data = data, coordinates
-
-rfi_location = [[0,0],[1,-3], [-5,6],[10,11]]
-rfi_deviation = [1,10,2,20]
-rfi_start_index = [40,200,200,100]
-rfi_snr = [50,40, 30, 30]
-rfi_drift = [1,-1, 1, 3]
-rfi_width = [10,5, 10,20]
-rfi_mean = 10
-RFI = [rfi_location, rfi_deviation, rfi_start_index, rfi_snr, rfi_drift,  rfi_width, rfi_mean]
-
-
-SETI_INDEX = [0,20]
-seti_start_index = [100,150]
-seti_snr = [30,30]
-seti_drift = [-2,2]
-seti_width = [30,30]
-SETI = [SETI_INDEX, seti_start_index, seti_snr, seti_drift,  seti_width]
-
-
+RFI = random_RFI_params(10)
+SETI = random_SETI_params(2)
+print("Test background Obs")
 obs2 = Observation(num_beams=64,
                  fchans=256,
                  tchans=16,
@@ -63,3 +37,14 @@ obs2 = Observation(num_beams=64,
                  RFI = RFI,
                  SETI = SETI,
                  obs_data=obs_data)
+print("batched simulation true") 
+create_simulated_obs_true_simulated()
+print("batched simulation false") 
+create_simulated_obs_false_simulated()
+print("batched simulation single true") 
+create_simulated_obs_single_true_simulated()
+print("passed")
+print("test vectorize")
+# vectorize(10, create_simulated_obs_true_simulated)
+# vectorize(create_simulated_obs_false_simulated, 10)
+# vectorize(create_simulated_obs_true_simulated, 10)
